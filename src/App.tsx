fb27abfa70ac59138599ce56d81a4da4e0b4da28
naokiuma <sagalumaata@gmail.com>
2022-07-19 01:20:38 +0900

    データを画面に表示まで。

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,55 @@
 
 import {useState} from 'react';
 
-import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
 import Results from './components/Results';
+import './App.css';
+
+type ResultsStateType = {
+  country:string;
+  cityName:string;
+  temprature:string;
+  conditionText:string;
+  icon:string;
+}
 
 
 
 
 function App() {
   const [city,setCity] = useState<String>("");
+  const [results,setResults] = useState<ResultsStateType>({
+    country:"",
+    cityName:"",
+    temprature:"",
+    conditionText:"",
+    icon:"",
+  });
+
   const getWeather = (e:any) => {
       e.preventDefault();
       fetch("https://api.weatherapi.com/v1/current.json?key=5c2df4c00423492cb1985434221707&q=tokyo&aqi=no")
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => {
+        setResults({
+          country: data.location.name,
+          cityName: data.location.name,
+          temprature: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+          
+        })
+      });
   }
   return (
     <div className="App">
      <Title />
      <Form setCity={setCity} getWeather={getWeather}/>
-     <Results />
+     <Results results={results}/>
+     {/* 慣例として、propsの名前は値と同じにする。 */}
+
+     {city}
 
     </div>
   );
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,24 @@
-const Results = () =>{
+type ResultsPropsType = {
+    //これらの方は、定義している箇所でマウスオーバーすることで確認できる。
+    results:{
+        country:string;
+        cityName:string;
+        temprature:string;
+        conditionText:string;
+        icon:string;
+    }
+    
+}
+
+const Results = (props:ResultsPropsType) =>{
+
     return (
-        <h1>気象データ</h1>
+        <>
+            <h1>気象データ</h1>
+            {props.results.country}<br/>
+            {props.results.temprature}度
+        </>
+        
     );
 }
 
